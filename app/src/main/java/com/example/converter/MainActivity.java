5783cd6b14be421741d9468abf3936f795e9449f
stormymayday <stormymayday7@gmail.com>
2024-05-21 17:41:25 -0700

    Implemented the Convert method

--- app/src/main/java/com/example/converter/MainActivity.java
+++ app/src/main/java/com/example/converter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.converter;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,5 +26,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Convert(View view) {
+
+        // Number Input Reference
+        EditText editText = findViewById(R.id.editTextNumber2);
+
+        // Text Label Reference
+        TextView textView = findViewById(R.id.textView);
+
+        // Reading the value stored in the Input Field
+        String dollar = editText.getText().toString();
+
+        // Checking if 'dollar' is not an empty string
+        if(!dollar.equals("")) {
+            // Converting dollar to euro
+            Float euro = Float.valueOf(dollar) * 0.85f;
+
+            // Displaying the result
+            textView.setText(euro.toString());
+        } else {
+            // Changing the TextView text
+            textView.setText(R.string.you_did_not_enter_any_value);
+        }
+
     }
 }
